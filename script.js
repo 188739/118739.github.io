18f855d9b521920f1d624066b201d5588c00aa85
hidde118739 <112405439+hidde118739@users.noreply.github.com>
2023-10-01 12:50:36 +0200

    .

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('openButton').addEventListener('click', function() {
+    document.getElementById('videoContainer').style.display = 'block';
+});
+
+document.getElementById('closeButton').addEventListener('click', function() {
+    document.getElementById('videoContainer').style.display = 'none';
+});
